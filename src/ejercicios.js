4ba56d0145cba90a34a7b760806fc0192bbfb5b7
MattiCosen <mattocosen@hotmail.com>
2021-04-03 15:41:45 -0300

    agrego template al html

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -3,3 +3,10 @@
 const contenedor = document.getElementById("contenedor");
 
 const lista = document.createDocumentFragment();
+
+const ul = document.createElement("ul");
+lista.appendChild(ul);
+
+const template = document.getElementById("temp").content;
+
+contenedor.appendChild(lista);
